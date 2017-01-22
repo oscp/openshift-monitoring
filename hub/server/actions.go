2511f1032ed6e5627ca678df3135daa97f6fd0f8
Reto Lehmann <reto.lehmann@me.com>
2017-01-22 15:40:11 +0100

    start jobs

--- hub/server/actions.go
+++ hub/server/actions.go
@@ -4,6 +4,7 @@ import (
 	"github.com/SchweizerischeBundesbahnen/openshift-monitoring/models"
 	"log"
 	"github.com/cenkalti/rpc2"
+	"github.com/mitchellh/mapstructure"
 )
 
 func deamonLeave(h *Hub, host string) {
@@ -19,4 +20,21 @@ func deamonJoin(h *Hub, d *models.Deamon, c *rpc2.Client) {
 	h.deamons[d.Hostname] = models.DeamonClient{Client:c, Deamon: *d}
 
 	h.toUi <- models.BaseModel{WsType: models.WS_NEW_DEAMON, Message: d.Hostname}
+}
+
+func newJob(h *Hub, msg interface{}) models.BaseModel {
+	var job models.Job
+	err := mapstructure.Decode(msg, &job)
+	if err != nil {
+		panic(err)
+	}
+
+	h.lastJobId++
+	job.JobId = h.lastJobId
+	h.jobs = append(h.jobs, job)
+
+	h.toDeamons <- job
+
+	// Return ok to UI
+	return models.BaseModel{WsType: models.WS_NEW_JOB, Message: job.JobId}
 }
