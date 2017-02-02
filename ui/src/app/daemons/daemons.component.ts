49c7491fb18d80e07f8aff2ffd4f7123c1d48503
u220374 <reto.lehmann@sbb.ch>
2017-02-02 08:00:29 +0100

    UI Improvements

--- ui/src/app/daemons/daemons.component.ts
+++ ui/src/app/daemons/daemons.component.ts
@@ -4,37 +4,39 @@ import {SocketType} from '../shared/socket.types';
 import {NotificationsService} from 'angular2-notifications';
 
 @Component({
-  selector: 'app-daemon-overview',
-  templateUrl: './daemons.component.html'
+    selector: 'app-daemon-overview',
+    templateUrl: './daemons.component.html'
 })
 export class DaemonsComponent implements OnInit {
-  private daemons: any;
+    private daemons: any;
 
-  constructor(private socketService: SocketService, private notificationService: NotificationsService) {
-    this.getDaemons();
-  }
+    constructor(private socketService: SocketService, private notificationService: NotificationsService) {
+        this.getDaemons();
+    }
 
-  ngOnInit() {
-    this.socketService.websocket.subscribe(
-      msg => {
-        let data = JSON.parse(msg.data);
-        switch (data.Type) {
-          case SocketType.ALL_DAEMONS:
-            this.daemons = data.Message.sort();
-            break;
-          case SocketType.NEW_DAEMON:
-            this.notificationService.info('Daemon joined', 'New daemon joined: ' + data.Message);
-            this.getDaemons();
-            break;
-          case SocketType.DAEMON_LEFT:
-            this.notificationService.info('Daemon left', 'Daemon left: ' + data.Message);
-            this.getDaemons();
-        }
-      }
-    );
-  }
+    ngOnInit() {
+        this.socketService.websocket.subscribe(
+            msg => {
+                let data = JSON.parse(msg.data);
+                switch (data.Type) {
+                    case SocketType.ALL_DAEMONS:
+                        this.daemons = data.Message.sort((a, b) => {
+                            return a.Hostname > b.Hostname ? 1 : ((b.Hostname > a.Hostname) ? -1 : 0);
+                        });
+                        break;
+                    case SocketType.NEW_DAEMON:
+                        this.notificationService.info('Daemon joined', 'New daemon joined: ' + data.Message);
+                        this.getDaemons();
+                        break;
+                    case SocketType.DAEMON_LEFT:
+                        this.notificationService.info('Daemon left', 'Daemon left: ' + data.Message);
+                        this.getDaemons();
+                }
+            }
+        );
+    }
 
-  getDaemons() {
-    this.socketService.websocket.next({Type: SocketType.ALL_DAEMONS});
-  }
+    getDaemons() {
+        this.socketService.websocket.next({Type: SocketType.ALL_DAEMONS});
+    }
 }
