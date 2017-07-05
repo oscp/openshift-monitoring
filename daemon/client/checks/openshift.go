db57262a8aa38130aa9b7a4c1beb771b8edea0f8
u220374 <reto.lehmann@sbb.ch>
2017-07-05 07:44:17 +0200

    Added retry to 'oc get nodes' check

--- daemon/client/checks/openshift.go
+++ daemon/client/checks/openshift.go
@@ -1,16 +1,17 @@
 package checks
 
 import (
-	"strings"
 	"bytes"
+	"errors"
+	"fmt"
 	"log"
 	"os/exec"
 	"strconv"
-	"errors"
-	"fmt"
+	"strings"
+	"time"
 )
 
-func CheckMasterApis(urls string) (error) {
+func CheckMasterApis(urls string) error {
 	log.Println("Checking master apis. At least one has to be up")
 
 	urlArr := strings.Split(urls, ",")
@@ -21,35 +22,53 @@ func CheckMasterApis(urls string) (error) {
 		if err := checkHttp(u); err == nil {
 			oneApiOk = true
 		} else {
-			msg += u + " is not reachable. ";
+			msg += u + " is not reachable. "
 		}
 	}
 
-	if (oneApiOk) {
+	if oneApiOk {
 		return nil
 	} else {
 		return errors.New(msg)
 	}
 }
 
-func CheckOcGetNodes() (error) {
+func CheckOcGetNodes() error {
 	log.Println("Checking oc get nodes output")
 
+	out, err := runOcGetNodes()
+	if err != nil {
+		return err
+	}
+
+	if strings.Contains(out, "NotReady") {
+		// Wait a few seconds and see if still NotReady
+		// to avoid wrong alerts
+		time.Sleep(10 * time.Second)
+
+		out2, err := runOcGetNodes()
+		if err != nil {
+			return err
+		}
+		if strings.Contains(out2, "NotReady") {
+			return errors.New("Some node is not ready! 'oc get nodes' output contained NotReady. Output: " + out2)
+		}
+	}
+
+	return nil
+}
+
+func runOcGetNodes() (string, error) {
 	out, err := exec.Command("bash", "-c", "oc get nodes --show-labels | grep -v monitoring=false | grep -v SchedulingDisabled").Output()
 	if err != nil {
 		msg := "Could not parse oc get nodes output: " + err.Error()
 		log.Println(msg)
-		return errors.New(msg)
+		return "", errors.New(msg)
 	}
-
-	if (strings.Contains(string(out), "NotReady")) {
-		return errors.New("Some node is not ready! 'oc get nodes' output contained NotReady")
-	} else {
-		return nil
-	}
+	return strings(out), nil
 }
 
-func CheckDnsNslookupOnKubernetes() (error) {
+func CheckDnsNslookupOnKubernetes() error {
 	log.Println("Checking nslookup to kubernetes ip")
 
 	cmd := exec.Command("nslookup", daemonDNSEndpoint, kubernetesIP)
@@ -64,38 +83,38 @@ func CheckDnsNslookupOnKubernetes() (error) {
 
 	stdOut := out.String()
 
-	if (strings.Contains(stdOut, "Server") && strings.Count(stdOut, "Address") >= 2 && strings.Contains(stdOut, "Name")) {
+	if strings.Contains(stdOut, "Server") && strings.Count(stdOut, "Address") >= 2 && strings.Contains(stdOut, "Name") {
 		return nil
 	} else {
 		return errors.New("Problem with dns to kubernetes. nsLookup had wrong output")
 	}
 }
 
-func CheckDnsServiceNode() (error) {
+func CheckDnsServiceNode() error {
 	log.Println("Checking dns to a openshift service")
 
 	ips := getIpsForName(daemonDNSServiceA)
 
-	if (ips == nil) {
+	if ips == nil {
 		return errors.New("Failed to lookup ip on node (dnsmasq) for name " + daemonDNSServiceA)
 	} else {
 		return nil
 	}
 }
 
-func CheckDnsInPod() (error) {
+func CheckDnsInPod() error {
 	log.Println("Checking dns to a openshift service inside a pod")
 
 	ips := getIpsForName(daemonDNSPod)
 
-	if (ips == nil) {
+	if ips == nil {
 		return errors.New("Failed to lookup ip in pod for name " + daemonDNSPod)
 	} else {
 		return nil
 	}
 }
 
-func CheckPodHttpAtoB() (error) {
+func CheckPodHttpAtoB() error {
 	log.Println("Checking if http connection does not work if network not joined")
 
 	// This should fail as we do not have access to this project
@@ -106,7 +125,7 @@ func CheckPodHttpAtoB() (error) {
 	return nil
 }
 
-func CheckPodHttpAtoC(slow bool) (error) {
+func CheckPodHttpAtoC(slow bool) error {
 	log.Println("Checking if http connection does work with joined network")
 
 	if err := checkHttp("http://" + daemonDNSServiceC + ":8090/" + getEndpoint(slow)); err != nil {
@@ -116,12 +135,12 @@ func CheckPodHttpAtoC(slow bool) (error) {
 	return nil
 }
 
-func CheckHttpService(slow bool) (error) {
+func CheckHttpService(slow bool) error {
 	errA := checkHttp("http://" + daemonDNSServiceA + ":8090/" + getEndpoint(slow))
 	errB := checkHttp("http://" + daemonDNSServiceB + ":8090/" + getEndpoint(slow))
 	errC := checkHttp("http://" + daemonDNSServiceC + ":8090/" + getEndpoint(slow))
 
-	if (errA != nil || errB != nil || errC != nil) {
+	if errA != nil || errB != nil || errC != nil {
 		msg := "Could not reach one of the services (a/b/c)"
 		log.Println(msg)
 		return errors.New(msg)
@@ -130,7 +149,7 @@ func CheckHttpService(slow bool) (error) {
 	return nil
 }
 
-func CheckHttpHaProxy(publicUrl string, slow bool) (error) {
+func CheckHttpHaProxy(publicUrl string, slow bool) error {
 	log.Println("Checking http via HA-Proxy")
 
 	if err := checkHttp(publicUrl + ":80/" + getEndpoint(slow)); err != nil {
@@ -140,7 +159,7 @@ func CheckHttpHaProxy(publicUrl string, slow bool) (error) {
 	return nil
 }
 
-func CheckRegistryHealth(ip string) (error) {
+func CheckRegistryHealth(ip string) error {
 	log.Println("Checking registry health")
 
 	if err := checkHttp("http://" + ip + ":5000/healthz"); err != nil {
@@ -150,7 +169,7 @@ func CheckRegistryHealth(ip string) (error) {
 	return nil
 }
 
-func CheckHawcularHealth(ip string) (error) {
+func CheckHawcularHealth(ip string) error {
 	log.Println("Checking metrics health")
 
 	if err := checkHttp("https://" + ip + ":443"); err != nil {
@@ -160,7 +179,7 @@ func CheckHawcularHealth(ip string) (error) {
 	return nil
 }
 
-func CheckRouterHealth(ip string) (error) {
+func CheckRouterHealth(ip string) error {
 	log.Println("Checking router health", ip)
 
 	if err := checkHttp("http://" + ip + ":1936/healthz"); err != nil {
@@ -170,7 +189,7 @@ func CheckRouterHealth(ip string) (error) {
 	return nil
 }
 
-func CheckLoggingRestartsCount() (error) {
+func CheckLoggingRestartsCount() error {
 	log.Println("Checking log-container restart count")
 
 	out, err := exec.Command("bash", "-c", "oc get pods -n logging -o wide | tr -s ' ' | cut -d ' ' -f 4").Output()
@@ -183,23 +202,23 @@ func CheckLoggingRestartsCount() (error) {
 	isOk := true
 	var msg string
 	for _, l := range strings.Split(string(out), "\n") {
-		if (!strings.HasPrefix(l, "RESTARTS") && len(strings.TrimSpace(l)) > 0) {
+		if !strings.HasPrefix(l, "RESTARTS") && len(strings.TrimSpace(l)) > 0 {
 			cnt, _ := strconv.Atoi(l)
-			if (cnt > 2) {
+			if cnt > 2 {
 				msg = "A logging-container has restart count bigger than 2 - " + strconv.Itoa(cnt)
 				isOk = false
 			}
 		}
 	}
 
-	if (!isOk) {
+	if !isOk {
 		return errors.New(msg)
 	} else {
 		return nil
 	}
 }
 
-func CheckRouterRestartCount() (error) {
+func CheckRouterRestartCount() error {
 	log.Println("Checking router restart count")
 
 	out, err := exec.Command("bash", "-c", "oc get po -n default | grep router | grep -v deploy | tr -s ' ' | cut -d ' ' -f 4").Output()
@@ -212,44 +231,44 @@ func CheckRouterRestartCount() (error) {
 	isOk := true
 	var msg string
 	for _, l := range strings.Split(string(out), "\n") {
-		if (!strings.HasPrefix(l, "RESTARTS") && len(strings.TrimSpace(l)) > 0) {
+		if !strings.HasPrefix(l, "RESTARTS") && len(strings.TrimSpace(l)) > 0 {
 			cnt, _ := strconv.Atoi(l)
-			if (cnt > 5) {
+			if cnt > 5 {
 				msg = "A Router has restart count bigger than 5 - " + strconv.Itoa(cnt)
 				isOk = false
 			}
 		}
 	}
 
-	if (isOk) {
+	if isOk {
 		return nil
 	} else {
 		return errors.New(msg)
 	}
 }
 
-func CheckEtcdHealth(etcdIps string, etcdCertPath string) (error) {
+func CheckEtcdHealth(etcdIps string, etcdCertPath string) error {
 	log.Println("Checking etcd health")
 
 	var msg string
 	isOk := true
 
-	if (len(etcdCertPath) > 0) {
+	if len(etcdCertPath) > 0 {
 		// Check etcd with custom certs path
 		isOk = checkEtcdHealthWithCertPath(&msg, etcdCertPath, etcdIps)
 
-		if (!isOk) {
+		if !isOk {
 			log.Println("etcd health check with custom cert path failed, trying with default")
 
 			// Check etcd with default certs path
-			isOk = checkEtcdHealthWithCertPath(&msg, "/etc/etcd/", etcdIps);
+			isOk = checkEtcdHealthWithCertPath(&msg, "/etc/etcd/", etcdIps)
 		}
 	} else {
 		// Check etcd with default certs path
-		isOk = checkEtcdHealthWithCertPath(&msg, "/etc/etcd/", etcdIps);
+		isOk = checkEtcdHealthWithCertPath(&msg, "/etc/etcd/", etcdIps)
 	}
 
-	if (!isOk) {
+	if !isOk {
 		return errors.New(msg)
 	} else {
 		return nil
@@ -257,8 +276,8 @@ func CheckEtcdHealth(etcdIps string, etcdCertPath string) (error) {
 }
 
 func checkEtcdHealthWithCertPath(msg *string, certPath string, etcdIps string) bool {
-	cmd := exec.Command("etcdctl", "--peers", etcdIps, "--ca-file", certPath + "ca.crt",
-		"--key-file", certPath + "peer.key", "--cert-file", certPath + "peer.crt", "cluster-health")
+	cmd := exec.Command("etcdctl", "--peers", etcdIps, "--ca-file", certPath+"ca.crt",
+		"--key-file", certPath+"peer.key", "--cert-file", certPath+"peer.crt", "cluster-health")
 
 	var out bytes.Buffer
 	cmd.Stdout = &out
@@ -270,7 +289,7 @@ func checkEtcdHealthWithCertPath(msg *string, certPath string, etcdIps string) b
 	}
 
 	stdOut := out.String()
-	if (strings.Contains(stdOut, "unhealthy") || strings.Contains(stdOut, "unreachable")) {
+	if strings.Contains(stdOut, "unhealthy") || strings.Contains(stdOut, "unreachable") {
 		*msg += "Etcd health check was 'cluster unhealthy'"
 		return false
 	}
@@ -278,7 +297,7 @@ func checkEtcdHealthWithCertPath(msg *string, certPath string, etcdIps string) b
 	return true
 }
 
-func CheckLimitsAndQuotas(allowedWithout int) (error) {
+func CheckLimitsAndQuotas(allowedWithout int) error {
 	log.Println("Checking limits & quotas")
 
 	// Count projects
@@ -312,10 +331,10 @@ func CheckLimitsAndQuotas(allowedWithout int) (error) {
 
 	log.Println("Parsed values (projects,limits,quotas)", pCount, lCount, qCount)
 
-	if (pCount - allowedWithout != lCount) {
+	if pCount-allowedWithout != lCount {
 		return errors.New("There are some projects without limits")
 	}
-	if (pCount - allowedWithout != qCount) {
+	if pCount-allowedWithout != qCount {
 		return errors.New("There are some projects without quotas")
 	}
 
