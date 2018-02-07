72fd17f332e89d23b35f7a9efa82186b8c55ad1c
u220374 <reto.lehmann@sbb.ch>
2018-02-07 13:41:42 +0100

    Bugfixes.

--- models/checks.go
+++ models/checks.go
@@ -3,7 +3,7 @@ package models
 type Checks struct {
 	IsRunning       bool   `json:"isRunning"`
 	CheckInterval   int    `json:"checkInterval"`
-	MasterApiCheck  bool   `json:"masterApiChecks"`
+	MasterApiCheck  bool   `json:"masterApiCheck"`
 	MasterApiUrls   string `json:"masterApiUrls"`
 	DnsCheck        bool   `json:"dnsCheck"`
 	HttpChecks      bool   `json:"httpChecks"`
