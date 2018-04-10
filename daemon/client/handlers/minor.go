841fdd4b6ef7ebef86fd91586c948c2c0f4f37da
b0rski <11145838+b0rski@users.noreply.github.com>
2018-04-10 16:39:50 +0200

    Kube config checks are now executed.

--- daemon/client/handlers/minor.go
+++ daemon/client/handlers/minor.go
@@ -23,12 +23,17 @@ func HandleMinorChecks(daemonType string, w http.ResponseWriter, r *http.Request
 
 	if daemonType == "MASTER" || daemonType == "NODE" {
 		certPaths := os.Getenv("CHECK_CERTIFICATE_PATHS")
+		kubePaths := os.Getenv("CHECK_CERTIFICATE_KUBE_PATHS")
 
-		if len(certPaths) == 0 {
-			log.Fatal("env variables 'CHECK_CERTIFICATE_PATHS' must be specified")
+		if len(certPaths) == 0 || len(kubePaths) == 0 {
+			log.Fatal("env variables 'CHECK_CERTIFICATE_PATHS', 'CHECK_CERTIFICATE_KUBE_PATHS' must be specified")
 		}
 
 		if err := checks.CheckFileSslCertificates(strings.Split(certPaths, ","), 80); err != nil {
+			errors = append(errors, err.Error())
+		}
+
+		if err := checks.CheckKubeSslCertificates(strings.Split(kubePaths, ","), 80); err != nil {
 			errors = append(errors, err.Error())
 		}
 	}
